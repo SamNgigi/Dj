d3c8e9c3389e0d46c5409e0af0a3e632ca81dadf
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-20 17:27:52 +0300

    successful fetch of data from django api

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,16 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class ApiTest extends React.Component{
+  constructor() {
+    // Seems we need super() for our this.state to work.
+    super();
+    this.state = {
+      'items': []
+    }
+  }
   componentDidMount(){
     this.getItems();
   }
-  getItems(){
+  getItems() {
     fetch('http://127.0.0.1:8000/Apis/item/')
      .then(results => results.json())
-     .then(results=> console.log(results));
+     .then(results=> this.setState({'items':results}));
   }
   render(){
-    return null;
+    return (
+
+      <ul>
+      <h1>Hii ndio shida</h1>
+          {this.state.items.map(function (item, index) {
+            return (
+              <div key={index}>
+                <h1>{item.title }</h1>
+                <p>{item.description}</p>
+              </div>
+            )
+          }
+        )}
+      </ul>
+    )
   }
 }
 
